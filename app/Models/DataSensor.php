cec18d3c01983662ca527269a89a725cb48f049b
IndroMan <indrodwi2@gmail.com>
2024-05-30 08:14:30 +0800

    API

--- app/Models/DataSensor.php
+++ app/Models/DataSensor.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class DataSensor extends Model
 {
     use HasFactory;
+    public $timestamps = true;
+    protected $fillable = ['device_id','temperature', 'humidity', 'light_intensity'];
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\MainController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/config/{deviceId}',[MainController::class, 'get_config']);
+Route::post('/data/{deviceId}',[MainController::class, 'post_data']);
